34c4eaefb3954762ef2e5de6b6601f9222d5dd1e
Enzo Orbe <enzoforbe@gmail.com>
2020-08-20 10:02:46 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    backendURL: "http://localhost:4000",
-    frontendURL: "http://localhost:3000",
+    backendURL: "https://immense-refuge-40769.herokuapp.com/",
+    frontendURL: "https://nodesend-cliente-gamma.vercel.app",
   },
 };
